1001b8ec8824b955bd1786dde1d10c5513829e11
ManishThapa <manishthapapani@gmail.com>
2022-07-06 14:39:01 +0200

    build-again

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import TopBar from './components/TopBar/TopBar';
 import SectionHeader from './components/SectionHeader/SectionHeader';
-import Services from './components/Services';
 import Shop from './components/Shop/Shop';
 import Navigation from './components/Navigation/Navigation';
 import ShopHeader from './components/ShopHeader/ShopHeader';
@@ -9,15 +8,14 @@ import MyTabs from './components/MyTabs/MyTabs';
 import Footer from './components/Footer/Footer';
 import OurTeamHeader from './components/OurTeamHeader/OurTeamHeader';
 
+
 function App() {
   return (
     <div className='App'>
       <TopBar/>
-      {/* <Navbar/> */}
       <Navigation/>
       <SectionHeader></SectionHeader>
       <MyTabs></MyTabs>
-      {/* <Services></Services> */}
       <ShopHeader></ShopHeader>
       <Shop/>
       <OurTeamHeader></OurTeamHeader>
--- src/components/SectionHeader/SectionHeader.js
+++ src/components/SectionHeader/SectionHeader.js
@@ -2,7 +2,7 @@ import './SectionHeader.css'
 function SectionHeader(){
     return(
         <div className='main_image'>
-            <img className='headerImage' id="main-img" src = "/assets/sectionHeader1.png" alt=''/>
+            <img className='shopImage' id="main-img" src = "/assets/sectionHeader1.png" alt=''/>
         </div>
     )
 }
