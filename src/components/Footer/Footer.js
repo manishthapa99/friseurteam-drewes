422e3cfa536de4b04a4809a5469b5c21a54c1107
ManishThapa <manishthapapani@gmail.com>
2022-07-06 15:02:01 +0200

    build-v2

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -14,7 +14,7 @@ return (
 	<Container>
 		<Row>
 		<Column>
-			<Heading>Quick Links V 1</Heading>
+			<Heading>Quick Links V 2</Heading>
             <FooterLink href="#">Home</FooterLink>
             <FooterLink href="#">Service</FooterLink>
             <FooterLink href="#">Shop</FooterLink>
--- src/components/SectionHeader/SectionHeader.js
+++ src/components/SectionHeader/SectionHeader.js
@@ -2,7 +2,7 @@ import './SectionHeader.css'
 function SectionHeader(){
     return(
         <div className='main_image'>
-            <img className='shopImage' id="main-img" src = "/assets/sectionHeader1.png" alt=''/>
+            <img className='shopImage' id="main-img" src = "/assets/SectionHeaderMain.png" alt=''/>
         </div>
     )
 }
